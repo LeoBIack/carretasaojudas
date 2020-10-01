b204c1875a36f4c5885b2814f0e08fbd1210bb29
Leonardo Preto <leonardofpreto@gmail.com>
2020-09-30 22:48:53 -0300

    Adicionando primeiros componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { Frame1Component } from './frame1/frame1.component';
+import { Frame2Component } from './frame2/frame2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    Frame1Component,
+    Frame2Component,
   ],
   imports: [
     BrowserModule
