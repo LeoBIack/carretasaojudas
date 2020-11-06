80ccd56287e89115a4f02388f1049e1d92427027
PedroRAlvarenga <pedralvarenga@gmail.com>
2020-11-06 10:23:53 -0300

    algumas mudanças

--- src/app/livros/livros-lista/livros-lista.component.ts
+++ src/app/livros/livros-lista/livros-lista.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Livro } from '../livro.model'
 import { LivroService } from '../livro.service';
-import { Observable, Subscription } from 'rxjs';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-livros-lista',
